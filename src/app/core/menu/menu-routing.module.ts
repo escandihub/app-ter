a074eb1e1d6b5ad2ef1b3da7654364e1f6260acf
Escandon <aleskndon@hotmail.com>
2020-09-27 15:04:41 -0500

    change children as lazyLoad

--- src/app/core/menu/menu-routing.module.ts
+++ src/app/core/menu/menu-routing.module.ts
@@ -10,19 +10,26 @@ const routes: Routes = [
     children: [
       {
         path:'inicio',
-         loadChildren: '../../modules/inicio/inicio.module#InicioPageModule'
+         loadChildren: () => 
+         import('../../modules/inicio/inicio.module').then(m => m.InicioPageModule)
       },
       {
         path: 'toro',
-        loadChildren: '../../modules/toro/toro.module#ToroPageModule'
+        // loadChildren: '../../modules/toro/toro.module#ToroPageModule'
+        loadChildren: () => 
+        import('../../modules/toro/toro.module').then(m => m.ToroPageModule)
       },
       {
         path: 'ternero',
-        loadChildren: '../../modules/ternero/ternero.module#TerneroPageModule'
+        // loadChildren: '../../modules/ternero/ternero.module#TerneroPageModule'
+        loadChildren: () => 
+        import('../../modules/ternero/ternero.module').then(m => m.TerneroPageModule)
       },
       {
         path: 'configuracion',
-        loadChildren: '../../modules/configuracion/configuracion.module#ConfiguracionPageModule'
+        // loadChildren: '../../modules/configuracion/configuracion.module#ConfiguracionPageModule'
+        loadChildren: () => 
+        import('../../modules/configuracion/configuracion.module').then(m => m.ConfiguracionPageModule)
       },
     ],
   },

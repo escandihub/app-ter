8ecf5129c123b48b5e7caf24ab38d2535ed3768e
Escandon <aleskndon@hotmail.com>
2020-09-27 15:08:03 -0500

    update rumiente model

--- src/app/share/models/rumiante.model.ts
+++ src/app/share/models/rumiante.model.ts
@@ -5,13 +5,13 @@ export interface Rumiante{
   nacimiento: string; //this is a date
   raza: string; 
   grupo: string;
-} 
+}; 
 
-export interface RumianteInsert{
-  id: number;
-  nombre: string;
-  sexo: boolean;
-  nacimiento: string; //this is a date
-  raza_id: number; 
-  grupo_id: number;
-} 
+// export interface RumianteInsert{
+//   id: number;
+//   nombre: string;
+//   sexo: boolean;
+//   nacimiento: string; //this is a date
+//   raza_id: number; 
+//   grupo_id: number;
+// } 

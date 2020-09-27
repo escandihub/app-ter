52acf61af0dff79a61d20e99941475b48c028cc7
Escandon <aleskndon@hotmail.com>
2020-09-27 09:30:36 -0500

    updating model

--- src/app/share/models/rumiante.model.ts
+++ src/app/share/models/rumiante.model.ts
@@ -1,8 +1,17 @@
-export interface Ternero{
+export interface Rumiante{
   id: number;
   nombre: string;
   sexo: boolean;
-  raza_id: number; //this is a date
-  nacimiento: string;
-  tipo: number;
+  nacimiento: string; //this is a date
+  raza: string; 
+  grupo: string;
+} 
+
+export interface RumianteInsert{
+  id: number;
+  nombre: string;
+  sexo: boolean;
+  nacimiento: string; //this is a date
+  raza_id: number; 
+  grupo_id: number;
 } 

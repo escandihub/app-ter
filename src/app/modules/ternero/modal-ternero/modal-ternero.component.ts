7798b389a047a12c15d5844d236741c037447948
Escandon <aleskndon@hotmail.com>
2020-09-27 23:44:05 -0500

    woking with a functionality that save the state of the props it's doest click save button

--- src/app/modules/ternero/modal-ternero/modal-ternero.component.ts
+++ src/app/modules/ternero/modal-ternero/modal-ternero.component.ts
@@ -1,21 +1,33 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 
+
 @Component({
   selector: 'app-modal-ternero',
   templateUrl: './modal-ternero.component.html',
   styleUrls: ['./modal-ternero.component.scss'],
 })
 export class ModalTerneroComponent implements OnInit {
-  @Input() data: object;
+   @Input()  data: object;
 
-  constructor(public modalCtrl: ModalController) { }
+  ternero = {};
 
-  ngOnInit() {}
+
+  constructor(
+    public modalCtrl: ModalController
+    ) { }
+
+  ngOnInit() {
+    this.ternero = {...this.data}
+  }
 
   dismiss(){
+    this.data = {...this.ternero}
     this.modalCtrl.dismiss({
       'dismissed': true
     });
+  }
+  onSubmit(){
+    
   }
 }

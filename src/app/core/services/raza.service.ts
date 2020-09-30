8e5879e893a5c383a4f2682d65f7f5e7a78e106a
Escandon <aleskndon@hotmail.com>
2020-09-30 09:54:37 -0500

    update services to use a mock data instend of sqlite service

--- src/app/core/services/raza.service.ts
+++ src/app/core/services/raza.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { BehaviorSubject, Observable, of } from 'rxjs';
 import { DatabaseService } from "./database.service";
 import { Raza } from "../../share/models/raza.model";
+import { HttpClient } from '@angular/common/http';
 
 /**
  * este servicio raza del rumiante
@@ -15,40 +16,57 @@ import { Raza } from "../../share/models/raza.model";
 export class RazaService {
 
   raza = new BehaviorSubject([]);
-  constructor(private db: DatabaseService) { 
+  constructor(private http: HttpClient) { 
+  // constructor(private db: DatabaseService) { 
   }
-  isOk(){
-    return this.db.isReady()
-  }
+  // isOk(){
+  //   return this.db.isReady()
+  // }
 
-  getRazaAS(){
-    return this.raza.asObservable();
-  }
-  loadRaza(){
-    let query = 'SELECT * FROM raza';
-    return this.db.database.executeSql(query, []).then(data => {
-      let raza: Raza[] = [];
+  // getRazaAS(){
+  //   return this.raza.asObservable();
+  // }
+  // loadRaza(){
+  //   let query = 'SELECT * FROM raza';
+  //   return this.db.database.executeSql(query, []).then(data => {
+  //     let raza: Raza[] = [];
       
-      if(data.rows.length > 0){
+  //     if(data.rows.length > 0){
 
-        for (let i = 0; i < data.rows.length; i++) {
-          console.log(data.rows.item(i).nombre);
-          raza.push({
-            id: data.rows.item(i).id,
-            nombre: data.rows.item(i).nombre,
-          });
-        }
-      }else{
-        console.log('sin datos :v');
-      }
-      this.raza.next(raza);
-    });
-  }
+  //       for (let i = 0; i < data.rows.length; i++) {
+  //         console.log(data.rows.item(i).nombre);
+  //         raza.push({
+  //           id: data.rows.item(i).id,
+  //           nombre: data.rows.item(i).nombre,
+  //         });
+  //       }
+  //     }else{
+  //       console.log('sin datos :v');
+  //     }
+  //     this.raza.next(raza);
+  //   });
+  // }
 
-  NewRaza(nombreRaza){
-    let query = 'INSERT INTO raza(nombre) VALUES(?)';
-    return this.db.database.executeSql(query, nombreRaza).then(data => {
-      this.loadRaza()
-    })
+  // NewRaza(nombreRaza){
+  //   let query = 'INSERT INTO raza(nombre) VALUES(?)';
+  //   return this.db.database.executeSql(query, nombreRaza).then(data => {
+  //     this.loadRaza()
+  //   })
+  // }
+
+  fakeGetData(): Observable<Raza[]>{
+        //return this.http.get('https://jsonplaceholder.typicode.com/todos/1');
+        let a = [
+          {
+            id:1,
+            nombre: 'BIL'
+          },
+          {
+            id:2,
+            nombre: 'MASO'
+          },
+        ]
+        
+    return of(a)
   }
 }

ad49ee242a4466cfeb9c0b0c45d5aa6e2abed270
Escandon <aleskndon@hotmail.com>
2020-09-30 09:50:14 -0500

    define the actions

---
+++ src/app/core/store/action.store.ts
@@ -0,0 +1,24 @@
+import { createAction, props, Action } from '@ngrx/store';
+import { Raza } from "../../share/models/raza.model";
+
+export enum RazaActionTypes {
+  AddRaza = '[Raza] add new Raza',
+  LoadRaza = '[Raza] Get razas from SQLite',
+  LoadSuccess = '[Raza] Load success'
+}
+
+export class AddRaza implements Action {
+  readonly type = RazaActionTypes.AddRaza;
+  constructor(public payload: { data: Raza[] }) { }
+}
+
+export class CargaSucces implements Action {
+  readonly type = RazaActionTypes.LoadSuccess
+  constructor(public payload: { data: Raza[] }) { }
+}
+
+export class GetRaza implements Action {
+  readonly type = RazaActionTypes.LoadRaza;
+}
+
+export type RazaActions = GetRaza | CargaSucces | AddRaza

01865686d8f663af9c144d26bab1470242c3f219
Escandon <aleskndon@hotmail.com>
2020-09-30 09:50:57 -0500

    define the effects to consume a fake service

---
+++ src/app/core/store/effects.store.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { Actions, createEffect, Effect, ofType } from '@ngrx/effects';
+import { Action } from '@ngrx/store'
+import { EMPTY, from, Observable, of } from 'rxjs';
+import { catchError, map, mergeMap } from 'rxjs/operators';
+import * as razaActions from '../store/action.store'
+import { RazaService } from '../services/raza.service';
+
+@Injectable()
+export class RazaEffects {
+  constructor(
+    private actions$: Actions,
+    private razaService: RazaService
+  ) { }
+
+  @Effect()
+  LoadRazas$: Observable<Action> = this.actions$.pipe(
+    ofType(razaActions.RazaActionTypes.LoadRaza),
+    mergeMap(
+      action => this.razaService.fakeGetData().pipe(
+        map(razas => (new razaActions.CargaSucces({ data: razas }))),
+        catchError(err => EMPTY)
+      )
+    )
+  )
+}

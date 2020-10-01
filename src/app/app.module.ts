59344350d77f4e7a623072721f5b7a1e3c3f19fe
Escandon <aleskndon@hotmail.com>
2020-10-01 15:19:55 -0500

    add datapicker | register new compoent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,10 +18,15 @@ import { EffectsModule } from '@ngrx/effects';
 
 import { RazaReduce } from "./core/store/reduce.store";
 import { RazaEffects } from './core/store/effects.store';
+
+import {NewRumianteComponent  } from "./share/components/new-rumiante/new-rumiante.component";
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { DatePicker } from '@ionic-native/date-picker/ngx';
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, NewRumianteComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule,
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule, FormsModule, ReactiveFormsModule,
     StoreModule.forRoot({razas: RazaReduce}),
     EffectsModule.forRoot([RazaEffects])
   ],
@@ -30,7 +35,8 @@ import { RazaEffects } from './core/store/effects.store';
     SplashScreen,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     SQLite, 
-    SQLitePorter
+    SQLitePorter,
+    DatePicker
   ],
   bootstrap: [AppComponent]
 })

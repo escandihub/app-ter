4d277752cfb315f30644eea8435f455f83e8332a
Escandon <aleskndon@hotmail.com>
2020-10-01 15:21:50 -0500

    Add modal and pass a props

--- src/app/modules/toro/toro.page.ts
+++ src/app/modules/toro/toro.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+
+import { NewRumianteComponent } from "../../share/components/new-rumiante/new-rumiante.component";
 
 @Component({
   selector: 'app-toro',
@@ -7,11 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ToroPage implements OnInit {
 
-  constructor() { }
+  constructor(public modalCtrl: ModalController) { }
 
   ngOnInit() {
   }
 
   // Aqui va a despachar el servicio que sera enviado al card-info
-
+  openNewToro(){
+    this.presentModal()
+  }
+  async presentModal() {
+    const modal = await this.modalCtrl.create({
+      component: NewRumianteComponent,
+      componentProps: {
+        whoIs: true
+      }
+    });
+    return await modal.present();
+  }
 }

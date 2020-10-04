a0b85ab92efb7930d6d36f9545094a873402d841
Escandon <aleskndon@hotmail.com>
2020-10-03 23:22:58 -0500

    add the the unique method

--- src/app/core/services/tipo.service.ts
+++ src/app/core/services/tipo.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { DatabaseService } from "./database.service";
 import { Tipo } from "../../share/models/tipo.model";
+import { resolve } from 'dns';
+import { rejects } from 'assert';
 
 /**
  * Este servicio se encarga de optener los 
@@ -9,6 +11,7 @@ import { Tipo } from "../../share/models/tipo.model";
  * la madre
  * el ternero
  * el macho 
+ * no se debe actualizar ni eliminar
  */
 
 @Injectable({
@@ -16,23 +19,24 @@ import { Tipo } from "../../share/models/tipo.model";
 })
 export class TipoService {
 
-   Tipos = new BehaviorSubject([]);
+  Tipos = new BehaviorSubject([]);
   constructor(private db: DatabaseService) { }
 
-  getTipos(){
+  getTipos() {
     let query = "SELECT * FROM tipos";
-
-    return this.db.database.executeSql(query, []).then(data => {
-      let tipos: Tipo[] = []
-      if(data.rows.lenght > 0){
-        for (let i = 0; i < data.rows.length; i++) {
-          tipos.push({
-            id: data.rows.item(i).id,
-            nombre: data.rows.item(i).nombe 
-          });
+    return new Promise((resolve, reject) => {
+      this.db.database.executeSql(query, []).then(data => {
+        let tipos: Tipo[] = []
+        if (data.rows.lenght > 0) {
+          for (let i = 0; i < data.rows.length; i++) {
+            tipos.push({
+              id: data.rows.item(i).id,
+              nombre: data.rows.item(i).nombe
+            });
+          }
         }
-      }
-      this.Tipos.next(tipos);
+        resolve(tipos)
+      }), (error => reject(error))
     })
   }
 }

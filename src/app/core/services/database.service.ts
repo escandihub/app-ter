69015ca97c73d0cf7410bce1cf77aaf6be06da51
Escandon <aleskndon@hotmail.com>
2020-10-05 09:29:05 -0500

    Becareful when you feed yout database this instance call's everytime that the app it's reloaded

--- src/app/core/services/database.service.ts
+++ src/app/core/services/database.service.ts
@@ -29,7 +29,7 @@ export class DatabaseService{
         this.database = db;
         //corremos script method
         console.log('alimentando');
-        this.seeder();
+        //this.seeder();
       });
     });
   }
